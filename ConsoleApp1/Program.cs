18c6c6dd6ece3ab66ae2ae5d5e12154ec1c8b4e4
Ahd3103 <Admin@DESKTOP-HTE2NRJ>
2023-02-16 16:25:16 +0530

    Adds new changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("just for git");
+        Console.WriteLine("Added aditional line to check the git");
+        //bchg
     }
 }
 
